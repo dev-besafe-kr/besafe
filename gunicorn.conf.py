75a445a3c4fc7dbdbdf6433e18c60fa58b894c79
HansolLim <dev.hansollim@gmail.com>
2023-07-10 20:10:58 +0900

    change to abstract path

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 bind = "0.0.0.0:8080"
 workers = 1
 name = "besafe"
-certfile = "~/.certbot/config/live/be-safe.kr/fullchain.pem"
-keyfile = "~/.certbot/config/live/be-safe.kr/privkey.pem"
+certfile = "/web/.certbot/config/live/be-safe.kr/fullchain.pem"
+keyfile = "/web/.certbot/config/live/be-safe.kr/privkey.pem"
