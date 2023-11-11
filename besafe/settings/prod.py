e4ba622b95aafa3e5b3987ed74427b1d2bb1faae
HansolLim <dev.hansollim@gmail.com>
2023-11-11 18:52:30 +0900

    SECURE_SSL_REDIRECT

--- besafe/settings/prod.py
+++ besafe/settings/prod.py
@@ -5,8 +5,8 @@ CSRF_TRUSTED_ORIGINS = [
     "https://be-safe.kr",
 ]
 DEBUG = False
-SECURE_SSL_REDIRECT = True
-SECURE_SSL_HOST = "be-safe.kr"
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# SECURE_SSL_REDIRECT = True
+# SECURE_SSL_HOST = "be-safe.kr"
+# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 MIDDLEWARE.append("django.middleware.cache.FetchFromCacheMiddleware")
