180c2b6fea3430dfb374cda3753766116fa4868b
HansolLim <dev.hansollim@gmail.com>
2023-12-27 14:51:34 +0900

    modal +

--- static/js/base.js
+++ static/js/base.js
@@ -16,4 +16,14 @@ window.addEventListener('load', () => {
     document.querySelectorAll('.animation').forEach(el => {
         setTimeout(() => observer.observe(el), 500);
     });
+
+    document.querySelectorAll("dialog").forEach(dialog => {
+        dialog.addEventListener('click', ({target: dialog}) => {
+            if (dialog.nodeName === 'DIALOG' && !dialog.getAttributeNames().find(n => n === "lock")) {
+                dialog.close('dismiss');
+            }
+        });
+
+        dialog.querySelectorAll(".close").forEach(el => el.addEventListener("click", () => dialog.close('dismiss')));
+    });
 });
--- static/js/slide.js
+++ static/js/slide.js
@@ -1,10 +1,9 @@
-window.addEventListener('load', function () {
-    const animationDelay = 500;
-    document.querySelectorAll(".slide").forEach((slide) => {
+const initSlides = () => {
+    const initSlide = slide => {
         const leftEl = slide.querySelector(".slide-left");
         const rightEl = slide.querySelector(".slide-right");
         const stopEl = slide.querySelector(".slide-stop");
-        leftEl && leftEl.addEventListener('click', () => {
+        const goLeft = () => {
             const ulEl = slide.querySelector(`.slide-ul`);
             const liEl = ulEl.querySelector('li');
             ulEl.scrollTo(ulEl.scrollLeft - liEl.clientWidth, 0);
@@ -21,8 +20,8 @@ window.addEventListener('load', function () {
                 });
                 prevEl.classList.add("active");
             }
-        });
-        rightEl && rightEl.addEventListener('click', () => {
+        };
+        const goRight = () => {
             const ulEl = slide.querySelector(`.slide-ul`);
             const liEl = ulEl.querySelector('li');
             ulEl.scrollTo(ulEl.scrollLeft + liEl.clientWidth + 20, 0);
@@ -39,31 +38,82 @@ window.addEventListener('load', function () {
                 });
                 nextEl.classList.add("active");
             }
-        });
+        };
+        leftEl && leftEl.addEventListener('click', goLeft);
+        rightEl && rightEl.addEventListener('click', goRight);
         stopEl && stopEl.addEventListener('click', () => {
             slide.classList.add("stop");
         });
 
-        // setInterval(() => {
-        //     if (slide.classList.contains("stop")) return;
-        //     rightEl && rightEl.click();
-        // }, 3000);
-    });
+        setInterval(() => {
+            if (slide.classList.contains("stop")) return;
+            goRight();
+        }, 3000);
+    };
+    document.querySelectorAll(".slide").forEach(initSlide);
+}
 
-    document.querySelectorAll(".shift").forEach(shift => {
+const initShifts = () => {
+    const animationDelay = 500;
+    const showShiftItem = (shift, handles, items, dataId) => {
+        shift.dataset.id = dataId;
         let isCurrentRevealed = false;
-        shift.querySelectorAll(".shift__handle").forEach(sel => {
-            const isCurrentItem = sel.classList.contains("active");
+        handles.forEach(sel => {
+            const isCurrentItem = sel.dataset.shift_id === dataId;
             if (isCurrentItem) {
+                sel.classList.add("active");
                 isCurrentRevealed = true;
+
+                const temp = sel;
+                setTimeout(() => {
+                    temp.classList.remove("prev", "next");
+                }, animationDelay);
             } else {
                 sel.classList.remove("prev", "next", "active");
                 sel.classList.add(isCurrentRevealed ? "next" : "prev");
             }
         });
-
         isCurrentRevealed = false;
-        shift.querySelectorAll(".shift__item").forEach(sel => {
+        items.forEach(sel => {
+            const isCurrentItem = sel.dataset.shift_id === dataId;
+            if (isCurrentItem) {
+                sel.classList.add("active");
+                isCurrentRevealed = true;
+
+                const temp = sel;
+                setTimeout(() => {
+                    temp.classList.remove("prev", "next");
+                }, animationDelay);
+            } else {
+                sel.classList.remove("prev", "next", "active");
+                sel.classList.add(isCurrentRevealed ? "next" : "prev");
+            }
+        });
+    }
+    const initShift = shift => {
+        let isCurrentRevealed = false;
+        const handles = Array.from(shift.querySelectorAll(".shift__handle"));
+        const items = Array.from(shift.querySelectorAll(".shift__item"));
+
+        let temp;
+        temp = shift.querySelector(".shift__handle.active");
+        if (!temp) {
+            if (handles.length > 0) {
+                temp = handles[0];
+                handles[0].classList.add("active");
+            }
+        }
+
+        temp = shift.querySelector(".shift__item.active");
+        if (!temp) {
+            if (items.length > 0) {
+                temp = handles[0];
+                items[0].classList.add("active");
+            }
+        }
+        shift.dataset.id = temp ? temp.dataset.shift_id : "1";
+
+        handles.forEach((sel, index) => {
             const isCurrentItem = sel.classList.contains("active");
             if (isCurrentItem) {
                 isCurrentRevealed = true;
@@ -71,55 +121,33 @@ window.addEventListener('load', function () {
                 sel.classList.remove("prev", "next", "active");
                 sel.classList.add(isCurrentRevealed ? "next" : "prev");
             }
+            sel.dataset.shift_id = sel.dataset.shift_id || (index + 1).toString();
         });
-        shift.querySelectorAll(".shift__handle").forEach(el => {
+        isCurrentRevealed = false;
+        items.forEach((sel, index) => {
+            const isCurrentItem = sel.classList.contains("active");
+            if (isCurrentItem) {
+                isCurrentRevealed = true;
+            } else {
+                sel.classList.remove("prev", "next", "active");
+                sel.classList.add(isCurrentRevealed ? "next" : "prev");
+            }
+            sel.dataset.shift_id = sel.dataset.shift_id || (index + 1).toString();
+        });
+
+        const shiftIdMap = items.map(i => i.dataset.shift_id);
+        handles.forEach(el => {
             el.addEventListener('click', () => {
-                const dataId = el.dataset.shift_id;
-                let isCurrentRevealed = false;
-                shift.querySelectorAll(".shift__handle").forEach(sel => {
-                    const isCurrentItem = sel.dataset.shift_id === dataId;
-                    if (isCurrentItem) {
-                        sel.classList.add("active");
-                        isCurrentRevealed = true;
-
-                        const temp = sel;
-                        setTimeout(() => {
-                            temp.classList.remove("prev", "next");
-                        }, animationDelay);
-                    } else {
-                        sel.classList.remove("prev", "next", "active");
-                        sel.classList.add(isCurrentRevealed ? "next" : "prev");
-                    }
-                });
-
-                isCurrentRevealed = false;
-                shift.querySelectorAll(".shift__item").forEach(sel => {
-                    const isCurrentItem = sel.dataset.shift_id === dataId;
-                    if (isCurrentItem) {
-                        sel.classList.add("active");
-                        isCurrentRevealed = true;
-
-                        const temp = sel;
-                        setTimeout(() => {
-                            temp.classList.remove("prev", "next");
-                        }, animationDelay);
-                    } else {
-                        sel.classList.remove("prev", "next", "active");
-                        sel.classList.add(isCurrentRevealed ? "next" : "prev");
-                    }
-                });
+                showShiftItem(shift, handles, items, el.dataset.shift_id);
             });
         });
-        const leftEl = shift.querySelector(".shift__left");
-        const rightEl = shift.querySelector(".shift__right");
-        leftEl && leftEl.addEventListener('click', () => {
-        });
-        rightEl && rightEl.addEventListener('click', () => {
-        });
+        shift.querySelectorAll(".shift__left").forEach(leftEl => leftEl.addEventListener('click', () => showShiftItem(shift, handles, items, shiftIdMap[shiftIdMap.indexOf(shift.dataset.id) - 1] || shift.dataset.id)))
+        shift.querySelectorAll(".shift__right").forEach(rightEl => rightEl.addEventListener('click', () => showShiftItem(shift, handles, items, shiftIdMap[shiftIdMap.indexOf(shift.dataset.id) + 1] || shift.dataset.id)))
+    };
+    document.querySelectorAll(".shift").forEach(initShift);
+}
 
-        setInterval(() => {
-            if (shift.classList.contains("stop")) return;
-            rightEl && rightEl.click();
-        }, 3000);
-    });
+window.addEventListener('load', function () {
+    initSlides();
+    initShifts();
 });
